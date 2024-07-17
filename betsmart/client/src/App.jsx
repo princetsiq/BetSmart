078521a951d162fff48e7d7cc1182f35a3b11ad3
MarcAnthonyWilliams <mw4725@nyu.edu>
2024-07-17 00:43:37 -0400

    Trying to commit/push again

--- betsmart/client/src/App.jsx
+++ betsmart/client/src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
+/*import { useState } from 'react'8 */
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -16,7 +14,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>BetSmart</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
