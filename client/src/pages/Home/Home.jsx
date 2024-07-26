c08f169d8ce037527ca49bc86db4255a1d20a48c
princetsiq <princetsiquaye@icloud.com>
2024-07-25 20:51:37 -0400

    Completed game/team views and sign up form

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ImageCarousel from '../../components/ImageCarousel/ImageCarousel.jsx'
-import './Home.css';
-// import './Home.scss';
+// import './Home.css';
+import './Home.scss';
 
 const Home = () => {
 	return (
